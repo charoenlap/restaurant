7a5518b5ab3eb62f3dd54e4c4aa636f2a2837434
charoenlap <charoenlap88@gmail.com>
2023-05-01 19:14:30 +0700

    test

--- public_html/catalog/view/theme/home.php
+++ public_html/catalog/view/theme/home.php
@@ -2,7 +2,11 @@
     <div class="row">
     <?php foreach ($tables->rows as $table) { ?>
         <div class="col-3">
-            <a href="<?php echo route('order&table_id='.$table['id']);?>" class="btn btn-<?php echo ((int)$table['table_status']==0?'secondary':'primary')?> btn-block mb-4"><?php echo $table['table_name'];?></a>
+            <?php if($table['hide']==1){?>
+                
+            <?php }else{?>
+                <a href="<?php echo route('order&table_id='.$table['id']);?>" class="btn btn-<?php echo ((int)$table['table_status']==0?'secondary':'primary')?> btn-block mb-4"><?php echo $table['table_name'];?></a>    
+            <?php }?>
         </div>    
     <?php } ?>
     </div>
