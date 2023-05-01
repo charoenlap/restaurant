a96411b75d1c903cb07b89884af10b52bcff2f87
tikkub <Tikkubzaza@gmail.com>
2023-05-01 22:02:37 +0700

    test

--- public_html/catalog/view/theme/order.php
+++ public_html/catalog/view/theme/order.php
@@ -65,7 +65,7 @@
                         <div class="form-group">
                             <label for="comment">Comment:</label>
                             <textarea class="form-control comment" id="comment" rows="3"></textarea>
-                            <a href="#" class="btn btn-primary comment-btn" data-text="ไม่ผัก">ไม่ผัก</a>
+                            <a href="#" class="btn btn-primary comment-btn" data-text="ไม่ผักกกกก">ไม่ผักกกกก</a>
                         </div>
                     </div>
                 </div>
