c7a4605e996d542124ee0f99d0a86195624a0ef9
charoenlap <charoenlap88@gmail.com>
2023-05-02 21:03:17 +0700

    a

--- public_html/catalog/view/theme/order.php
+++ public_html/catalog/view/theme/order.php
@@ -317,7 +317,7 @@
         // Code to handle delete button click goes here
         $('#confirmModal').modal('hide');
         $('#editModal').modal('hide');
-        
+         
         $.ajax({
             url: 'index.php?route=order/deleteOrder',
             type: 'POST',
